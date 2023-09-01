9e9723e37a50de01d94e3fb8cb33632259073ab3
xavatu <xavatu@gmail.com>
2023-09-01 13:50:09 +0300

    init alembic

--- config/db.py
+++ config/db.py
@@ -12,7 +12,7 @@ DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "cat_service")
+DB_NAME = os.getenv("DB_NAME", "catservice")
 DB_URL = (
     f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
